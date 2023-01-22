9902cea657c560a2c392c6d38fea7f1e85700f1d
Ethan <43922849+ethanppl@users.noreply.github.com>
2023-01-22 15:17:41 +0800

    Use regex instead

--- content_scripts.js
+++ content_scripts.js
@@ -1,6 +1,8 @@
+const shortsURLRegex = /http[s]?:\/\/www\.youtube\.com\/shorts/;
+
 const interval = setInterval(() => {
   for (node of document.querySelectorAll("ytd-grid-video-renderer")) {
-    if (node.querySelector("a#thumbnail").href.match("https://www.youtube.com/shorts/") !== null) {
+    if (node.querySelector("a#thumbnail").href.match(shortsURLRegex) !== null) {
       node.remove();
     }
   }
