101c89ae22b21e431b243e54b05a3ed892fd78e2
Ethan <43922849+ethanppl@users.noreply.github.com>
2023-01-23 22:11:35 +0800

    Remove shorts from home page

--- content_scripts.js
+++ content_scripts.js
@@ -1,9 +1,21 @@
 const shortsURLRegex = /http[s]?:\/\/www\.youtube\.com\/shorts/;
 
 const interval = setInterval(() => {
-  for (node of document.querySelectorAll("ytd-grid-video-renderer")) {
-    if (node.querySelector("a#thumbnail").href.match(shortsURLRegex) !== null) {
-      node.remove();
+  const url = new URL(document.URL);
+
+  if (url.pathname === "/") {
+    // home page
+    for (node of document.querySelectorAll("ytd-rich-section-renderer")) {
+      if (node.querySelector("ytd-rich-shelf-renderer").isShorts) {
+        node.remove();
+      }
+    }
+  } else if (url.pathname === "/feed/subscriptions") {
+    // subscriptions page
+    for (node of document.querySelectorAll("ytd-grid-video-renderer")) {
+      if (node.querySelector("a#thumbnail").href.match(shortsURLRegex) !== null) {
+        node.remove();
+      }
     }
   }
 }, 500);
